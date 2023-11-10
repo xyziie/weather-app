434267ba184882ca4328ca08ebb2b3724e7ad1c9
vishal.bulbule <vishal.bulbule@techtrapture.com>
2023-11-10 14:53:50 +0530

    Initial code

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+import requests
+import var
+
+app = Flask(__name__)
+
+def get_weather(api_key, city):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q': city,
+        'appid': api_key,
+        'units': 'metric'  # Use 'imperial' for Fahrenheit
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        data = response.json()
+
+        if response.status_code == 200:
+            return data
+        else:
+            return None
+
+    except Exception as e:
+        return None
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        city = request.form['city']
+        api_key = var.key
+        weather_data = get_weather(api_key, city)
+        return render_template('index.html', weather_data=weather_data)
+    
+    return render_template('index.html', weather_data=None)
+
+if __name__ == "__main__":
+    app.run(debug=True)
